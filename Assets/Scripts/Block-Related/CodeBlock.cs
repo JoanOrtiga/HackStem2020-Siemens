f55a673ceecf2c1c234bff041b898f762b4fbb2d
JoanStark <47462893+JoanStark@users.noreply.github.com>
2020-10-17 13:40:45 +0200

    UI, CodeBlocks, some scene examples.

---
+++ Assets/Scripts/Block-Related/CodeBlock.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeBlock : MonoBehaviour
+{
+    public virtual void UpdateObject(GameObject codedObject = null)
+    {
+        print("This shouldn't be called.");
+        print(codedObject.name);
+    }
+}
---
+++ Assets/Scripts/UI/DraggeableWindow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DraggeableWindow : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private GameObject panel;
+
+    private Color backgroundColor;
+
+    private bool spawn = false;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (!spawn)
+        {
+            Instantiate(gameObject, panel.transform);
+            spawn = true;
+        }
+
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnPointerDown()
+    {
+        dragRectTransform.SetAsLastSibling();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        print("Hola");
+    }
+}
