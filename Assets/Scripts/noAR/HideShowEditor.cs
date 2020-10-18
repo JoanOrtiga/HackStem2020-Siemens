1ef3d95b54249e0c35c1ab86ddc3c41fe663b201
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2020-10-18 13:51:59 +0200

    SCene

---
+++ Assets/Scripts/noAR/HideShowEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideShowEditor : MonoBehaviour
+{
+
+    public GameObject Editor;
+    public GameObject NoEditor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowEditor()
+    {
+        Editor.SetActive(true);
+        NoEditor.SetActive(false);
+    }
+
+    
+}
